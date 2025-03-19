586776e5614e93237ac0b290a838966031063793
James Anderson <james@tisteltech.com>
2025-03-19 12:51:52 -0400

    code gen
    start work on elixir

--- jea-code-gen-elixir.el
+++ jea-code-gen-elixir.el
@@ -52,52 +52,6 @@
 \"\"\"
 "))
 
-(defun jea-code-gen--elixir-ctor(name)
-	"Constructor boilerplate.  NAME is the class name."
-	(with-suppressed-warnings ()
-		(format "class %s:
-    \"\"
-
-    def __init__(self):
-        pass
-
-" (capitalize name))))
-
-(defun jea-code-gen--elixir-func(name &optional args)
-	"Function boilerplate set to NAME with optional ARGS."
-	(with-suppressed-warnings ()
-		(let ((params (if args (concat "self, " (string-join args ", "))
-										"self")))
-			(format "    def %s(%s):
-        \"\"
-        result = None
-        return result
-
-" name params))))
-
-(defun jea-code-gen--insert-class-elixir (name functions)
-	"Generate a class named NAME with the functions in the string FUNCTIONS.
-FUNCTIONS will look like (\"bark\", \"jump\", \"skip.\")"
-	(insert (jea-code-gen--elixir-preamble))
-	(insert (jea-code-gen--elixir-ctor name))
-	(dolist (f functions)
-		(insert (jea-code-gen--elixir-func f nil))))
-
-(defun jea-code-gen--insert-func-elixir (name args)
-	"Generate a function named NAME with the args from ARGS.
-AGRS will look like (\"bark\", \"jump\", \"skip.\")"
-	(insert (jea-code-gen--elixir-func name args)))
-
-;; (with-current-buffer (get-buffer-create "*jea-code-gen*")
-;;   (erase-buffer)
-;; ;;   (jea-code-gen-class "dog" '("sleep" "bark" "dig" "swim")))
-;; 	(jea-code-gen--func-elixir "dog" '("sleep" "bark" "dig" "swim")))
-
-(defun jea-code-gen-elixir()
-	"Turn on elixir code gen.  Set local funcs to the global vars."
-	(setf jea-code-gen-make-class-func 'jea-code-gen--insert-class-elixir)
-	(setf jea-code-gen-make-func-func 'jea-code-gen--insert-func-elixir)
-	t)
 
 (provide 'jea-code-gen-elixir)
 
