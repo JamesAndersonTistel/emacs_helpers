83f5e01a582a58119994535b0c214b8f6bd64a7e
James Anderson <james@tisteltech.com>
2025-03-19 12:51:20 -0400

    code gen
    start work on nestjs

--- jea-code-gen-nestjs.el
+++ jea-code-gen-nestjs.el
@@ -52,52 +52,6 @@
 \"\"\"
 "))
 
-(defun jea-code-gen--nestjs-ctor(name)
-	"Constructor boilerplate.  NAME is the class name."
-	(with-suppressed-warnings ()
-		(format "class %s:
-    \"\"
-
-    def __init__(self):
-        pass
-
-" (capitalize name))))
-
-(defun jea-code-gen--nestjs-func(name &optional args)
-	"Function boilerplate set to NAME with optional ARGS."
-	(with-suppressed-warnings ()
-		(let ((params (if args (concat "self, " (string-join args ", "))
-										"self")))
-			(format "    def %s(%s):
-        \"\"
-        result = None
-        return result
-
-" name params))))
-
-(defun jea-code-gen--insert-class-nestjs (name functions)
-	"Generate a class named NAME with the functions in the string FUNCTIONS.
-FUNCTIONS will look like (\"bark\", \"jump\", \"skip.\")"
-	(insert (jea-code-gen--nestjs-preamble))
-	(insert (jea-code-gen--nestjs-ctor name))
-	(dolist (f functions)
-		(insert (jea-code-gen--nestjs-func f nil))))
-
-(defun jea-code-gen--insert-func-nestjs (name args)
-	"Generate a function named NAME with the args from ARGS.
-AGRS will look like (\"bark\", \"jump\", \"skip.\")"
-	(insert (jea-code-gen--nestjs-func name args)))
-
-;; (with-current-buffer (get-buffer-create "*jea-code-gen*")
-;;   (erase-buffer)
-;; ;;   (jea-code-gen-class "dog" '("sleep" "bark" "dig" "swim")))
-;; 	(jea-code-gen--func-nestjs "dog" '("sleep" "bark" "dig" "swim")))
-
-(defun jea-code-gen-nestjs()
-	"Turn on nestjs code gen.  Set local funcs to the global vars."
-	(setf jea-code-gen-make-class-func 'jea-code-gen--insert-class-nestjs)
-	(setf jea-code-gen-make-func-func 'jea-code-gen--insert-func-nestjs)
-	t)
 
 (provide 'jea-code-gen-nestjs)
 
