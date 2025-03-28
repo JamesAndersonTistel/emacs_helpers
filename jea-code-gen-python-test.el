c7f83708bf084168af58005d0e80f24ace7cdff3
James Anderson <james@tisteltech.com>
2025-03-27 21:49:53 -0400

    code gen
    start work making a unit test system
    
    move macros to different file

--- jea-code-gen-python-test.el
+++ jea-code-gen-python-test.el
@@ -3,26 +3,15 @@
 ;;; Code:
 
 (require 'jea-code-gen-python)
+(require 'jea-code-gen-test)
 
 ;; need to setup a proper unit test system soon
 ;; just moving the test code out
+;; START here need to be more systematic and bring over each func.
+;; pass in debug as a arg with default off
 
-;; TODO write a string diff thay high lights the difference! char by char.
-
-(defmacro jea-test-text (func expected-text-output)
-	"Run the function FUNC and double check the text output is EXPECTED-TEXT-OUTPUT."
-	`(let* ((debug t) ;; turn on prints
-					(output (funcall ,func))
-					(success (string-equal output ,expected-text-output)))
-		 (if success
-				 (progn
-					 (if debug
-							 (message "success %s" ,func))
-					 t)
-			 (progn
-				 (if debug
-						 (message "failed because %s: '%s' does not equal '%s'." ,func output ,expected-text-output))
-				 nil))))
+;; (defun jea-code-gen-test-python()
+;; 	(let (
 
 (jea-test-text '(lambda () (jea-cg--py-ctor "cat" '(("sleep" "str") ("bark" "int") ("dig" "bool"))))
 "class Cat:
@@ -36,41 +25,16 @@
 
 ")
 
-
-(jea-cg--py-variables-split '("ssleep" "ibark" "bdig"))
-;; (("sleep" "str") ("bark" "int") ("dig" "bool"))
-
-(jea-cg--py-ctor "cat" '(("sleep" "str") ("bark" "int") ("dig" "bool")))
-"class Cat:
-    \"\"
-
-    def __init__(self, sleep: str, bark: int, dig: bool):
-        self._sleep = sleep
-        self._bark = bark
-        self._dig = dig
+(jea-test-list '(lambda () (jea-cg--py-variables-split '("ssleep" "ibark" "bdig")))
+							 '(("sleep" "str") ("bark" "int") ("dig" "bool")))
 
 
-")
+;; (jea-cg--py-dict-kv-fmt "foo" "bar" nil nil)
+;; (jea-cg--py-dict-kv-fmt "foo" 14 nil nil)
+;; (jea-cg--py-dict-kv-fmt "bar" 14 nil t)
+;; (jea-cg--py-dict-kv-fmt "foo" 15.4 nil t)
 
-;; "class Cat:
-;;     \"\"
-;;
-;;     def __init__(self, sleep: str, bark: int, dig: bool):
-;;         self._sleep = sleep
-;;         self._bark = bark
-;;         self._dig = dig
-;;
-;;
-;; "
-
-
-(jea-cg--py-dict-kv-fmt "foo" "bar" nil nil)
-(jea-cg--py-dict-kv-fmt "foo" 14 nil nil)
-(jea-cg--py-dict-kv-fmt "bar" 14 nil t)
-(jea-cg--py-dict-kv-fmt "foo" 15.4 nil t)
-
-
-(jea-cg--py-dict "d1" '("one" "2" "three" "four" "five" "6"))
+;; (jea-cg--py-dict "d1" '("one" "2" "three" "four" "five" "6"))
 
 (provide 'jea-code-gen-python-test)
 
---
+++ jea-code-gen-test.el
@@ -0,0 +1,39 @@
+;;; jea-code-gen-test.el --- unit test the code gen
+
+;;; Code:
+
+;; TODO write a string diff thay high lights the difference! char by char.
+
+(defmacro jea-test-text (func expected-text-output)
+	"Run the function FUNC and double check the text output is EXPECTED-TEXT-OUTPUT."
+	`(let* ((debug nil) ;; turn on prints
+					(output (funcall ,func))
+					(success (string-equal output ,expected-text-output)))
+		 (if success
+				 (progn
+					 (if debug
+							 (message "success %s" ,func))
+					 t)
+			 (progn
+				 (if debug
+						 (message "failed because %s: '%s' does not equal '%s'." ,func output ,expected-text-output))
+				 nil))))
+
+(defmacro jea-test-list (func expected-list-output)
+	"Run the function FUNC and double check the text output is EXPECTED-LIST-OUTPUT."
+	`(let* ((debug t) ;; turn on prints
+					(output (funcall ,func))
+					(success (equal output ,expected-list-output)))
+		 (if success
+				 (progn
+					 (if debug
+							 (message "success %s" ,func))
+					 t)
+			 (progn
+				 (if debug
+						 (message "failed because %s: '%s' does not equal '%s'." ,func output ,expected-list-output))
+				 nil))))
+
+(provide 'jea-code-gen-test)
+
+;;; jea-code-gen-test.el ends here
