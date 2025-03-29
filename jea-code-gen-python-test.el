b91fe3d0e630590b9310e09c6a4b1032bbf6088b
James Anderson <james@tisteltech.com>
2025-03-29 16:54:12 -0400

    code gen python
    add unit tests
    
    will need to chain the files together in the future

--- jea-code-gen-python-test.el
+++ jea-code-gen-python-test.el
@@ -12,7 +12,35 @@
 
 (defun jea-code-gen-test-python()
 	"Test all python functions."
-	(let ((t1 (jea-test-text '(lambda () (jea-cg--py-ctor "cat" '(("sleep" "str") ("bark" "int") ("dig" "bool"))))
+	(let ((t1 (jea-test-list '(lambda ()
+															(jea-cg--py-variable-split "icount"))
+													 '("count" "int")))
+				(t2 (jea-test-list '(lambda ()
+															(jea-cg--py-variable-split "fweight"))
+													 '("weight" "float")))
+				(t3 (jea-test-list '(lambda ()
+															(jea-cg--py-variable-split "balive"))
+													 '("alive" "bool")))
+				(t4 (jea-test-list '(lambda ()
+															(jea-cg--py-variable-split "sname"))
+													 '("name" "str")))
+				(t5 (jea-test-list '(lambda ()
+															(jea-cg--py-variable-split "ucount"))
+													 '("count" "union")))
+				(t6 (jea-test-list '(lambda ()
+															(jea-cg--py-variables-split
+															 '("ssleep" "ibark" "bdig" "sswim")))
+													 '(("sleep" "str") ("bark" "int") ("dig" "bool") ("swim" "str"))))
+				(t7 (jea-test-text '(lambda ()
+															(jea-cg--py-variable-fmt "height" "float" nil nil))
+													 " height: float,")) ;; note leading space
+				(t8 (jea-test-text '(lambda ()
+															(jea-cg--py-variable-fmt "height" "float" t nil))
+													 "height: float,"))
+				(t9 (jea-test-text '(lambda ()
+															(jea-cg--py-variable-fmt "height" "float" nil t))
+													 " height: float")) ;; note the trailing comma is gone
+				(t10 (jea-test-text '(lambda () (jea-cg--py-ctor "cat" '(("sleep" "str") ("bark" "int") ("dig" "bool"))))
 "class Cat:
     \"\"
 
@@ -23,19 +51,59 @@
 
 
 "))
-				(t2 (jea-test-list '(lambda () (jea-cg--py-variables-split '("ssleep" "ibark" "bdig")))
-													 '(("sleep" "str") ("bark" "int") ("dig" "bool")))))
-		(and t1 t2)))
+				(t11 (jea-test-text '(lambda ()
+															 (jea-cg--py-func "hello"
+																								'(("sleep" "str") ("bark" "int") ("dig" "bool") ("swim" "str"))))
+"    def hello(self, sleep: str, bark: int, dig: bool, swim: str):
+        \"\"
+        result = None
+        return result
 
-;; (jea-code-gen-test-python)
+"))
+				(t12 (jea-test-text '(lambda () (jea-cg--py-func "hello"))
+"    def hello(self):
+        \"\"
+        result = None
+        return result
 
-;; (jea-cg--py-dict-kv-fmt "foo" "bar" nil nil)
-;; (jea-cg--py-dict-kv-fmt "foo" 14 nil nil)
-;; (jea-cg--py-dict-kv-fmt "bar" 14 nil t)
-;; (jea-cg--py-dict-kv-fmt "foo" 15.4 nil t)
+"))
+				(t13 (jea-test-text '(lambda () (jea-cg--py-add--compare "x" 2)) "x == 2"))
+				(t14 (jea-test-text '(lambda () (jea-cg--py-add--compare "y" "bar")) "y == 'bar'"))
+				(t15 (jea-test-text '(lambda () (jea-cg--py-switch "x" '(1 2 3)))
+"    if x == 1:
+        pass
+    elif x == 2:
+        pass
+    elif x == 3:
+        pass
+    else:
+        pass
+"))
+				(t16 (jea-test-text '(lambda () (jea-cg--py-switch "x" '("dog" "cat" 3)))
+"    if x == 'dog':
+        pass
+    elif x == 'cat':
+        pass
+    elif x == 3:
+        pass
+    else:
+        pass
+"))
+				(t17 (jea-test-text '(lambda ()
+															 (jea-cg--py-dict "d1"
+																								'("a" "1" "bee" "42.33" "see" "far")))
+"d1 = {
+    'a': 1,
+    'bee': 42.33,
+    'see': 'far'
+}
+")))
+	
+		(and t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11 t12 t13 t14 t15 t16 t17)))
 
-;; (jea-cg--py-dict "d1" '("one" "2" "three" "four" "five" "6"))
+(jea-code-gen-test-python)
 
 (provide 'jea-code-gen-python-test)
 
 ;;; jea-code-gen-python-test.el ends here
+
