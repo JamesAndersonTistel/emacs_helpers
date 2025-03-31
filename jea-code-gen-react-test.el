7eb72aac4fc7a88f1ae31b8a8ffa85bc2d0cd71b
James Anderson <james@tisteltech.com>
2025-03-31 09:02:24 -0400

    code gen unit test react
    first test

--- jea-code-gen-react-test.el
+++ jea-code-gen-react-test.el
@@ -13,39 +13,35 @@
 (defun jea-code-gen-test-react()
 	"Test all react functions."
 	(let ((t1 (jea-test-text '(lambda ()
-															(jea-cg--js-dict "d1"
-																							 '("one" "2" "three" "four" "five" "6")))
-"let d1 = {
-    one: 2,
-    three: 'four',
-    five: 6
-};
-"))
-				(t2 (jea-test-text '(lambda ()
-															(jea-cg--js-switch "pet" '("dog" "cat" "fish")))
-"    switch(pet) {
-    case 'dog':
-        break;
-    case 'cat':
-        break;
-    case 'fish':
-        break;
-    default:
-        break;
-    }"))
-				(t3 (jea-test-text '(lambda ()
-															(jea-cg--js-switch "petCount" '(1 2 3)))
-"    switch(petCount) {
-    case 1:
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    default:
-        break;
-    }")))
-		(and t1 t2 t3)))
+															(jea-cg--react-class "Game"
+																									 '(("sleep" "str") ("bark" "int") ("dig" "bool"))))
+"// Copyright © 2025 James Anderson
+//
+// Author: James Anderson <james@tisteltech.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files \\(the \"Software\"\\), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so.
+//
+// THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import { useState } from \"react\";
+
+export default function Game() {
+    const [sleep, setSleep] = useState(\"\");
+    const [bark, setBark] = useState(0);
+    const [dig, setDig] = useState(false);
+
+    return (<></>);
+}")))
+		(and t1)))
 
 (jea-code-gen-test-react)
 
