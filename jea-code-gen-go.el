28b655dcbe61d88e721235f748e67d1da2e7756e
James Anderson <james@tisteltech.com>
2025-04-14 14:40:41 -0400

    code gen for go start

---
+++ jea-code-gen-go.el
@@ -0,0 +1,72 @@
+;;; jea-code-gen-go.el --- code to break long strings into 280 character chunks
+
+;; Copyright © 2025 James Anderson
+;;
+;; Author: James Anderson <james@tisteltech.com>
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy of this
+;; software and associated documentation files (the "Software"), to deal in the Software
+;; without restriction, including without limitation the rights to use, copy, modify,
+;; merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+;; INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+;; PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;; Generate boiler late code for go3.
+
+;;; Code:
+
+(require 'jea-code-gen)
+(require 'jea-string-util)
+
+(defun jea-cg--go-preamble()
+	"Start of file preamble text."
+	(with-suppressed-warnings ()
+		(format "# Copyright © %s James Anderson
+#
+# Author: James Anderson <james@tisteltech.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files \\(the \"Software\"\\), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+\"\"\"
+\"\"\"
+" (format-time-string "%Y" (current-time)))))
+
+(defun jea-code-gen-use-go()
+	"Turn on go code gen.  Set local funcs to the global vars."
+	(interactive)
+	(setf jea-code-gen-make-class-func
+				'(lambda (name functions) (message "not implemented yet.")))
+	(setf jea-code-gen-make-func-func
+				'(lambda (name functions) (message "not implemented yet.")))
+	(setf jea-code-gen-make-switch-func
+				'(lambda (name functions) (message "not implemented yet.")))
+	(setf jea-code-gen-make-dict-func
+				'(lambda (name functions) (message "not implemented yet.")))
+	(setf jea-code-gen-make-api-get-func
+				'(lambda (name functions) (message "not implemented yet.")))
+	t)
+
+(provide 'jea-code-gen-go)
+
+;;; jea-code-gen-go.el ends here
